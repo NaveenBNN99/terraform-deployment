572b2bd92f881fe8d8ad87fa71587919b6c2d913
NaveenBNN99 <100353935+NaveenBNN99@users.noreply.github.com>
2023-08-29 13:33:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,16 +3,6 @@ variable "region" {
   default = "us-east-1"
 }
 
-/*variable "access_key"{
-    type = string
-    default = "AKIAQWAG4O2D4KF5SKAR"
-}
-
-variable "secret_key" {
-    type = string
-    default = "mrXZ50gmM5trJHY702vcAYv4vB4VCHLb3mUtbjtK"
-  
-}*/
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
